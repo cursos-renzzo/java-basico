e27e7859932529e71a96e0eff41a6520ee1e3181
Renzzo Rocha <renzzorocha17@gmail.com>
2024-08-21 12:07:56 -0300

    feat: add an system-hour app
    getting the system-hour by `java` libs.

---
+++ 2-hora-do-sistema/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Date data = new Date();
+
+        System.out.println("A hora do sistema é: " + data.toString());
+    }
+}
