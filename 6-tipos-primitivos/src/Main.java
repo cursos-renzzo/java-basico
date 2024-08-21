c10955facd882af71b4b85d81663a07ab0d07c8a
Renzzo Rocha <renzzorocha17@gmail.com>
2024-08-21 15:35:32 -0300

    feat: add primitive data types in `Java`
    learning about primitive data types in java and applying those concepts in a question...

---
+++ 6-tipos-primitivos/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        float nota = 8.5f;
+        System.out.println("A nota é " + nota);
+        System.out.print("A nota é " + nota);
+
+        int numero = 10;
+        System.out.println(numero);
+
+        double teste = 333.333;
+        System.out.println(teste);
+
+    }
+}
