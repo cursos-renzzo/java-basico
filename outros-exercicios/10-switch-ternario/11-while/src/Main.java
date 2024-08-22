00102a3b97172fc962966c95bec159c41c4929cc
Renzzo Rocha <renzzorocha17@gmail.com>
2024-08-22 17:05:13 -0300

    feat: add a while ex
    learning while and repetitive structures

---
+++ outros-exercicios/10-switch-ternario/11-while/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i < 10) {
+            i++;
+            System.out.println(i);
+            if(i == 4){
+                break;
+            }
+        }
+
+    }
+
+}
