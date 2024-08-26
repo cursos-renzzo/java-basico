f4f4798027cd6c865ff106ef53329141dc803708
Renzzo Rocha <renzzorocha17@gmail.com>
2024-08-26 10:17:51 -0300

    feat: add a array test
    learning arrays

---
+++ outros-exercicios/10-switch-ternario/14-vetores/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] n = {3, 4, 5, 6, 7};
+//
+//        for(int c = 0; c <= 4; c++){
+//            System.out.println(n[c]);
+
+//        int[] v = {20, 10, 30, 40, 50};
+//        Arrays.sort(v);
+//        for (int valor : v) {
+//            System.out.println(valor);
+//        }
+
+//        int[] vet = {3,1,2,4,5,6,9,8};
+//        int p = Arrays.binarySearch(vet, 9);
+//        System.out.println(p);
+
+          int[] v = new int[20];
+          Arrays.fill(v, 20);
+          for(int valor: v)
+        System.out.println(Arrays.toString(v));
+
+    }
+
+}
+
